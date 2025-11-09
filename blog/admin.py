c0b8478c0c976943aedbce1ba2f98cb72da993cc
AbgahV18 <abgash_vv@fizmat.tnpu.edu.ua>
2025-11-09 18:14:09 +0100

    Add views, urls and templates for blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "created_date", "published_date")
+    list_filter = ("created_date", "published_date", "author")
+    search_fields = ("title", "text")
+    ordering = ("-created_date",)
